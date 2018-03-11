e6f64e339ec326febadb5f646e2d4441d07c826c
Gion Kunz <gion.kunz@gmail.com>
2018-03-11 10:48:50 -0700

    Lab 4 code

---
+++ src/app/service/todo.service.ts
@@ -0,0 +1,51 @@
+import {Injectable} from '@angular/core';
+import {TodoItem} from '../model/todo';
+
+@Injectable()
+export class TodoService {
+  private todos: TodoItem[] = [{
+    nr: '1',
+    title: 'Todo 1',
+    description: 'Todo 1 Description',
+    done: true
+  }, {
+    nr: '2',
+    title: 'Todo 2',
+    description: 'Todo 2 Description',
+    done: false
+  }, {
+    nr: '3',
+    title: 'Todo 3',
+    description: 'Todo 3 Description',
+    done: false
+  }];
+
+  getTodos(): TodoItem[] {
+    return this.todos;
+  }
+
+  getTodo(nr: string): TodoItem {
+    return this.todos.find((todo) => todo.nr === nr) || <TodoItem>{};
+  }
+
+  private getNextNr() {
+    return `${
+      this.todos.reduce((n, todo) => +todo.nr > +n ? +todo.nr : +n, 0) + 1
+    }`;
+  }
+
+  createTodo(data: any): void {
+    this.todos.push({
+      nr: this.getNextNr(),
+      done: false,
+      ...data
+    });
+  }
+
+  updateTodo(nr: string, data: any): void {
+    const index = this.todos.findIndex((todo) => todo.nr === nr);
+    if (index !== -1) {
+      Object.assign(this.todos[index], data);
+    }
+  }
+}
