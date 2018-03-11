8823504609ad609c1659a4a7d549ce3d75679b57
Gion Kunz <gion.kunz@gmail.com>
2018-03-11 12:14:54 -0700

    Lab 5 solution

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,6 +1,5 @@
-import {Component, EventEmitter, Inject, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {TodoItem} from '../../model/todo';
-import {TodoService} from '../../service/todo.service';
 
 @Component({
   selector: 'app-todo-list',
@@ -8,28 +7,10 @@ import {TodoService} from '../../service/todo.service';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent {
-  // TODO: Use input and output in simple UI components
-  // Make this property an Input property using the @Input decorator
-  // You can then bind the todos from the TodoListContainer component
-  // onto this input.
-  todoItems: TodoItem[];
+  @Input() todoItems: TodoItem[];
   @Output() outMarkAsDone = new EventEmitter<TodoItem>();
 
-  // TODO: No dependencies in simple UI components
-  // Since we're no longer manipulating data within our TodoList UI
-  // component, we don't need the TodoService here anymore
-  constructor(@Inject(TodoService) private todoService: TodoService) {
-    this.todoItems = todoService.getTodos();
-  }
-
   markAsDone(todoItem: TodoItem) {
-    // TODO: Use outMarkAsDone output instead
-    // Our TodoList should be a simple UI component .We'd like to
-    // use the output outMarkAsDone to emit an event instead of
-    // doing data manipulation directly.
-    this.todoService.updateTodo(todoItem.nr, {
-      done: !todoItem.done
-    });
-    this.todoItems = this.todoService.getTodos();
+    this.outMarkAsDone.emit(todoItem);
   }
 }
